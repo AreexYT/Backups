77f46996a17c7f0e5f97653e2f6196de3fa6c109
Searching4Myeslf <88655156+AreexYT@users.noreply.github.com>
2022-08-22 13:23:25 -0700

    Create RemoteSpy.lua

---
+++ FE Scipts/RemoteSpy.lua
@@ -0,0 +1,9 @@
+local settings = {
+   saveCalls = false,
+   maxCallsSaved = 1000,
+   saveOnlyLastCall = true,
+   maxTableDepth = 100,
+   minimizeBind = Enum.KeyCode.RightAlt,
+   blacklistedNames = {}
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AreexYT/Backups/main/FE%20Scipts/Local%20Files/RemoteSpyFunctions.lua"))(settings)
